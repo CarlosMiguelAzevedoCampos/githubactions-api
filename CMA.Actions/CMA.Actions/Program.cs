4611d1f5899f0f172c9af97c8fd7ebc244d5f799
Carlos Miguel Campos <carlos.miguel.campos@conti-engineering.com>
2022-08-19 14:53:46 +0100

    + PR to be created.

--- CMA.Actions/CMA.Actions/Program.cs
+++ CMA.Actions/CMA.Actions/Program.cs
@@ -3,6 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/test", () => "Hello Carlos!");
-app.MapGet("/carlos", () => "Hello Carlos!");
+app.MapGet("/carlos", () => "Hello Carlos! ");
 
 app.Run();
